55dd347ed8bc07da2e9f7c07e16cfea02bc0fbc1
psilva88 <arthurpsilva@aluno.colegiomotiva.com.br>
2025-05-07 13:39:16 -0300

    Update and rename main_ouvidoria_teste.java to main_biblioteca_teste.java

---
+++ main_biblioteca_teste.java
@@ -0,0 +1,12 @@
+// •🔍Listagem de todos os livros registrados;
+
+// •➕Adicionar uma novo livro e armazená-lo no sistema;
+
+// •📊Exibir a quantidade total de livros registradas;
+
+// •🔎Pesquisar uma livro específico através de um código único;
+
+// •🗑️Excluir uma livro pelo código de identificação;
+
+// •🚪Sair do sistema de gerenciamento de biblioteca de forma segura.
+
